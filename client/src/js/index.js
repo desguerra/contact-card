cb1218d32db4a59524a995be366947472fcb81b3
desguerra <ddesguerra@ucdavis.edu>
2022-11-06 17:05:44 -0800

    finish form submit

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,6 @@
 /* webpack entry point */
 import  "./form";
-import "./submit";
+// import "./submit"; // uncomment to disable form submit
 
 import "../css/index.css";
 /* Bootstrap's npm module uses Popper as a peer dependency. 
@@ -12,18 +12,54 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { initdb, getDb, postDb } from './database';
 
+import { fetchCards } from './cards';
+import { toggleForm, clearForm } from './form';
+
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
   initdb();
+  fetchCards();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
+});
 
-  // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
-  getDb();
-  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
-  getDb();
+
+// Form functionality //
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+})
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+
+    fetchCards();
+      // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
 });
