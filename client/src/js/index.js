0f355e71ebe88b8c00b4b8b49cb694ff24467474
desguerra <ddesguerra@ucdavis.edu>
2022-11-06 21:32:23 -0800

    update and delete functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,7 +10,7 @@ needed dependencies will be imported too. */
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { initdb, getDb, postDb } from './database';
+import { initdb, getDb, postDb, deleteDb, editDb } from './database';
 
 import { fetchCards } from './cards';
 import { toggleForm, clearForm } from './form';
@@ -26,6 +26,7 @@ window.addEventListener('load', function () {
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
 });
+
 
 
 // Form functionality //
@@ -51,6 +52,14 @@ form.addEventListener('submit', event => {
     postDb(name, email, phone, profile);
   } else {
 
+    // Obtains values passed into the form element
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+    // Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+    editDb(profileId, name, email, phone, profile);
+
     fetchCards();
       // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
@@ -63,3 +72,31 @@ form.addEventListener('submit', event => {
   // Reload the DOM
   fetchCards();
 });
+
+window.deleteCard = (e) => {
+  // Grabs the id from the button element attached to the contact card.
+  let id = parseInt(e.id);
+  // Delete the card
+  deleteDb(id);
+  // Reload the DOM
+  fetchCards();
+};
+
+window.editCard = (e) => {
+  // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+  profileId = parseInt(e.dataset.id);
+
+  // Grabs information to pre-populate edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+
+  form.style.display = "block";
+
+  // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+    submitBtnToUpdate = true;
+};
