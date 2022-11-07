8f9225687a8a1940006aee5e4711c3b5ad624708
desguerra <ddesguerra@ucdavis.edu>
2022-11-06 16:33:25 -0800

    connect indexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -10,11 +10,14 @@ needed dependencies will be imported too. */
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { initdb } from './database';
+
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
+  initdb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
